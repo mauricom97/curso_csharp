f058dc0eed427cb0a15cd0af4328805034e2e6d5
Mauricio Nunes <mauricionunesdasilvanunes@gmail.com>
2023-07-22 00:44:42 -0300

    Variaveis

---
+++ aula3/Aula03.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Aula03
+{
+    static void Main() {
+        int num1,num2,res;
+
+
+        num1 = 2;
+        num2 = 5;
+        res = num1 * num2;
+
+
+        Console.WriteLine("A multiplicação de "+num1+" mais "+num2+" é igual a " + res);
+
+    }
+}
