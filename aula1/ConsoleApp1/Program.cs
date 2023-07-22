9e10ab67af6ccf61ebe5e59c5ea6ddabe3d1b96e
Mauricio Nunes <mauricionunesdasilvanunes@gmail.com>
2023-07-22 00:22:24 -0300

    first commit

---
+++ aula1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Olá Mundo \n");
+        Console.Write("Mauricio Nunes \n");
+        Console.Write("Theo Henrique de Melo da Silva \n");
+        Console.ReadLine();
+    }
+}
