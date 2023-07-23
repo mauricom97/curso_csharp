8ee7632f7c3a6d55bfff030d369ed68cda624504
Mauricio Nunes <mauricionunesdasilvanunes@gmail.com>
2023-07-23 11:58:34 -0300

    Enumeradores (enum)

---
+++ aula10/Aula10.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Aula10 {
+        enum DaysWeek { Domingo, Segunda, Terca, Quarta, Quinta, Sexta, Sabado };
+    static void Main() {
+        DaysWeek ds = DaysWeek.Terca;
+        Console.WriteLine(ds);
+
+    }
+}
